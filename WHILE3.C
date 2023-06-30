e53194969f44dd40afa402c5fca46c247883a866
bhuvankachhadiya <137286464+bhuvankachhadiya@users.noreply.github.com>
2023-06-30 16:27:56 +0530

    Add files via upload

---
+++ WHILE3.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+int a=1,n;
+clrscr();
+printf("plz enter proper value of n=");
+scanf("%d",&n);
+while(a<=n)
+{
+ printf("%d\n",a);
+ a++;
+}
+
+getch();
+}
